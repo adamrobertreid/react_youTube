7903b8623dd943402dac7fdacca95c6e3a5dc10f
adamrobertreid <adamrobertreid@gmail.com>
2016-10-31 18:35:01 -0700

    adding controlled components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';   // import react from react
 
 
-class SearchBar extends Component{
-  render() {
-    return <input onChange={event => console.log(event.target.value)}; />;
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+}
+
+
+render() {
+    return (
+    <div>
+      <input
+        value={this.state.term}
+        onChange={event => this.setState({ term: event.target.value})} />
+    </div>
+    );
   }
 }
 
